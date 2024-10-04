eee6b4588d4ee8e9562609211aeffa3d2bd52028
Liliana <liliana.aguridan@gmail.com>
2024-10-04 10:27:48 +0300

    LoginNegativeTests - beautify

--- src/test/java/LoginNegativeTests.java
+++ src/test/java/LoginNegativeTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import test.data.Constants;
 
 public class LoginNegativeTests {
 
@@ -11,7 +12,7 @@ public class LoginNegativeTests {
 
     @BeforeTest
     public void setup() {
-        driver.get("https://magento.softwaretestingboard.com/");
+        driver.get(Constants.BASE_URL);
         driver.manage().window().maximize();
     }
 
@@ -36,7 +37,8 @@ public class LoginNegativeTests {
         String actualMessage1 = driver.findElement(By.xpath("//div[@id='pass-error']")).getText();
         String expectedMessage1 = "This is a required field.";
         Assert.assertEquals(actualMessage1, expectedMessage1);
-        String actualColor1 = driver.findElement(By.xpath("//div[@id='pass-error']")).getCssValue("color");
+        String actualColor1 = driver.findElement(By.xpath("//div[@id='pass-error']"))
+                .getCssValue("color");
         String expectedColor1 = "rgb(224, 43, 39)";
         Assert.assertEquals(actualColor1, expectedColor1);
     }
@@ -51,7 +53,8 @@ public class LoginNegativeTests {
         String actualMessage2 = driver.findElement(By.xpath("//div[@id='email-error']")).getText();
         String expectedMessage2 = "This is a required field.";
         Assert.assertEquals(actualMessage2, expectedMessage2);
-        String actualColor2 = driver.findElement(By.xpath("//div[@id='email-error']")).getCssValue("color");
+        String actualColor2 = driver.findElement(By.xpath("//div[@id='email-error']"))
+                .getCssValue("color");
         String expectedColor2 = "rgb(224, 43, 39)";
         Assert.assertEquals(actualColor2, expectedColor2);
     }
@@ -66,7 +69,8 @@ public class LoginNegativeTests {
         String actualMessage3 = driver.findElement(By.xpath("//div[@id='email-error']")).getText();
         String expectedMessage3 = "Please enter a valid email address (Ex: johndoe@domain.com).";
         Assert.assertEquals(actualMessage3, expectedMessage3);
-        String actualColor3 = driver.findElement(By.xpath("//div[@id='email-error']")).getCssValue("color");
+        String actualColor3 = driver.findElement(By.xpath("//div[@id='email-error']"))
+                .getCssValue("color");
         String expectedColor3 = "rgb(224, 43, 39)";
         Assert.assertEquals(actualColor3, expectedColor3);
     }
@@ -74,11 +78,13 @@ public class LoginNegativeTests {
     @Test(testName = "TC04 - Valid email invalid password")
     public void validEmailInvalidPasswordTest() throws InterruptedException {
         driver.findElement(By.xpath("(//a[contains(text(), 'Sign In')])[1]")).click();
-        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("ione17.popescu@gmail.com");
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys(
+                "ione17.popescu@gmail.com");
         driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("invalid_password");
         Thread.sleep(2000);
         driver.findElement(By.xpath("(//button[@id='send2'])[1]")).click();
-        String actualMessage4 = driver.findElement(By.xpath("//div[@class='messages']")).getCssValue("color");
+        String actualMessage4 = driver.findElement(By.xpath("//div[@class='messages']"))
+                .getCssValue("color");
         String expectedMessage4 = "rgb(51, 51, 51)";
         Assert.assertEquals(actualMessage4, expectedMessage4);
     }
@@ -93,13 +99,15 @@ public class LoginNegativeTests {
         String actualMessage5 = driver.findElement(By.xpath("//div[@id='email-error']")).getText();
         String expectedMessage5 = "This is a required field.";
         Assert.assertEquals(actualMessage5, expectedMessage5);
-        String actualColor5 = driver.findElement(By.xpath("//div[@id='email-error']")).getCssValue("color");
+        String actualColor5 = driver.findElement(By.xpath("//div[@id='email-error']"))
+                .getCssValue("color");
         String expectedColor5 = "rgb(224, 43, 39)";
         Assert.assertEquals(actualColor5, expectedColor5);
         String actualMessage6 = driver.findElement(By.xpath("//div[@id='pass-error']")).getText();
         String expectedMessage6 = "This is a required field.";
         Assert.assertEquals(actualMessage6, expectedMessage6);
-        String actualColor6 = driver.findElement(By.xpath("//div[@id='pass-error']")).getCssValue("color");
+        String actualColor6 = driver.findElement(By.xpath("//div[@id='pass-error']"))
+                .getCssValue("color");
         String expectedColor6 = "rgb(224, 43, 39)";
         Assert.assertEquals(actualColor6, expectedColor6);
     }
