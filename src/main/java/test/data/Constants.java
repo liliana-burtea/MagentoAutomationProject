4e2d48e2f07612cc7254d026d56ca0cf3f00efde
Liliana <liliana.aguridan@gmail.com>
2024-10-04 08:14:56 +0300

    TestClass1 beautify

---
+++ src/main/java/test/data/Constants.java
@@ -0,0 +1,20 @@
+package test.data;
+
+public class Constants {
+
+    // public = ofer vizibilitate in alte clase
+    // static = ofera acces folosind numele clasei
+    // final = transforma o variabila in constanta
+    // Denumirea de constanta este doar cu litere mari si poate folosi undrscore
+
+    public static final String BASE_URL = "https://magento.softwaretestingboard.com/";
+    public static final String BLUE_COLOR = "rgb(25, 121, 195)";
+    public static final String LIGHT_BLUE_COLOR = "rgb(0, 173, 226)";
+
+    public static final String BACKGROUND_COLOR_CSS = "background-color";
+    public static final String EMAIL = "marinela.popescu@gmail.com";
+    public static final String PASSWORD = ".Testpass.123";
+
+
+
+}
--- src/test/java/TestClass1.java
+++ src/test/java/TestClass1.java
@@ -1,35 +1,54 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
+import test.data.Constants;
 
 public class TestClass1 {
 
-    @Test
+    @Test(
+            testName = "TC01 - User open Firefox browser - Magento homepage",
+            description =
+                    """
+                    Step 1: User Open Firefox browser
+                    Step 2: User navigates to Magento homepage
+                    Step 3: User open Magento homepage successfuly
+                    """
+    )
     public void goToWebsiteTest() {
         // Primul pas este sa deschid un browser
         // Pentru browser selenium ne ofera obiectul WebDriver
         WebDriver driver = new FirefoxDriver();
         // Pentru a naviga pe un link se foloseste metodsa .get
-        driver.get("https://magento.softwaretestingboard.com/");
+        driver.get(Constants.BASE_URL);
         driver.close();
     }
 
-    @Test
+    @Test(
+            testName = "TC02 - User is login to Magento homepage",
+            description =
+                    """
+                    Step 1: User navigates to Magento homepage
+                    Step 2: User click on login button
+                    Step 3: User type valid email address
+                    Step 4: User type valid password
+                    Step 5: User click on Sign in button
+                    Step 6: User is logged in successfuly
+                    """
+    )
     public void loginTest() {
         WebDriver driver = new FirefoxDriver();
-        driver.get("https://magento.softwaretestingboard.com/");
+        driver.get(Constants.BASE_URL);
         driver.manage().window().maximize();
         // Formula generica a unui XPath
         // //tag[@atribut='valoare']
         // Formula generica a unui XPath cu contains
         // //tag[contains(text(), 'textul cautat')]
         driver.findElement(By.xpath("(//a[contains(text(), 'Sign In')])[1]")).click();
-        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("marinela.popescu@gmail.com");
-        driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(".Testpass.123");
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys(Constants.EMAIL);
+        driver.findElement(By.xpath("//input[@id='pass']")).sendKeys(Constants.PASSWORD);
         driver.findElement(By.xpath("(//button[@id='send2'])[1]")).click(); // cand am mai multe elemente
-        // de acelasi fel in pagina, imbrac xpath-ul in paranteze rotund si deschid [] si pun indexul care incepe de la 1
+    // de acelasi fel in pagina, imbrac xpath-ul in paranteze rotund si deschid [] si pun indexul care incepe de la 1
     }
 }
 
